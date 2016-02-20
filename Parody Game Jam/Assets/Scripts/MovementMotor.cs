ea097322706bbe354dab79ce5b1e74d9a4bb2318
DomCristaldi <domeniccristaldi@gmail.com>
2016-02-20 15:49:14 -0500

    First pass of movement motor is in

---
+++ Parody Game Jam/Assets/Scripts/MovementMotor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementMotor : MonoBehaviour {
+
+    public Vector3 desiredDirec;
+    public Vector3 trueDirec;
+
+    public float redirectSpeed = 1.0f;
+
+    public float maxMoveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateTrueDirec();
+
+        ApplyMovement();
+	}
+
+    private void UpdateTrueDirec() {
+        if (trueDirec == desiredDirec) { return; }
+
+        trueDirec = Vector3.MoveTowards(trueDirec, desiredDirec, redirectSpeed * Time.deltaTime);
+    }
+
+    private void ApplyMovement() {
+        transform.Translate(trueDirec);
+    }
+}
