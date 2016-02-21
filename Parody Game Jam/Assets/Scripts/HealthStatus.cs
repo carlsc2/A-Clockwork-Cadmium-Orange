d2974de66e4dd31040e2a69fa63a23f1160665de
Craig Carlson <cjcarlson20@gmail.com>
2016-02-21 07:20:27 -0500

    status ui

---
+++ Parody Game Jam/Assets/Scripts/HealthStatus.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthStatus : MonoBehaviour {
+
+	public Image[] splats;
+	public Image face;
+	public Sprite[] faces;
+
+	public int health = 100;
+	private float maxHealth = 100;
+
+	public Text healthText;
+	public Text scoreText;
+
+	public long score = 0;
+
+
+	public void Hit() {
+		if (health > 0) {
+			health--;
+			int splatindex = (int)((1 - health / maxHealth) * (splats.Length-1));
+			int faceindex = (int)((1 - health / maxHealth) * (faces.Length-1));
+			splats[splatindex].enabled = true;
+			face.sprite = faces[faceindex];
+
+			healthText.text = "Health: " + Mathf.FloorToInt(health / maxHealth).ToString() + "%";
+		}
+	}
+
+	public void AddScore(long points) {
+		score += points;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+
+}
