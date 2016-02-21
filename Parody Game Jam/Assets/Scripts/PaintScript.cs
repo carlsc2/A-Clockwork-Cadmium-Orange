81918543701bf4558309cbc259f1fb98a7d3c4d3
Craig Carlson <cjcarlson20@gmail.com>
2016-02-20 20:05:16 -0500

    improved paint script

--- Parody Game Jam/Assets/Scripts/PaintScript.cs
+++ Parody Game Jam/Assets/Scripts/PaintScript.cs
@@ -14,54 +14,71 @@ public class PaintScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 	public Image drawingCanvas;
 	public RectTransform brush;
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.E)) {
-			print("ayy lmao");
-		}
+	public LookController playercontrol;
 
+	private bool dragging = false;
+
+	void Update() {
+		//have to check in update because can't drag if mouse is locked
+		if (Input.GetMouseButton(1)) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			UnityEditor.EditorApplication.isPaused = true;
+		}
 	}
 
 	public void OnBeginDrag(PointerEventData eventData) {
-		drawingCanvas.enabled = true;
-		startPosition = brush.position;
-		canvastex = new Texture2D(128, 128);
-		canvastex.filterMode = FilterMode.Point;
-		drawingCanvas.sprite = Sprite.Create(canvastex, new Rect(0, 0, 128, 128), new Vector2(0.5f, 0.5f));
-		prevpos = Input.mousePosition;
-
-		Color[] cols = new Color[128 * 128];
-		for (int i = 0; i < 128 * 128; i++) {
-			cols[i] = Color.clear;
+		if (eventData.button == PointerEventData.InputButton.Right) {
+			playercontrol.canLookAround = false;
+			drawingCanvas.enabled = true;
+			startPosition = brush.position;
+			canvastex = new Texture2D(128, 128);
+			canvastex.filterMode = FilterMode.Point;
+			drawingCanvas.sprite = Sprite.Create(canvastex, new Rect(0, 0, 128, 128), new Vector2(0.5f, 0.5f));
+			prevpos = Input.mousePosition;
+		
+			Color[] cols = new Color[128 * 128];
+			for (int i = 0; i < 128 * 128; i++) {
+				cols[i] = Color.clear;
+			}
+			canvastex.SetPixels(cols);
+			canvastex.Apply();
+			dragging = true;
 		}
-
-		canvastex.SetPixels(cols);
-		canvastex.Apply();
 	}
 
 	public void OnDrag(PointerEventData eventData) {
-		brush.position = Input.mousePosition;
-
-		Bresenham3D line = new Bresenham3D(prevpos, Input.mousePosition);
-
-		foreach(Vector3 point in line) {
-			Vector2 tmp = Camera.main.ScreenToViewportPoint(point);
-			canvastex.SetPixel((int)(tmp.x * 128), (int)(tmp.y * 128), Color.red);
+		if (dragging) {
+			brush.position = Input.mousePosition;
+			if (eventData.button == PointerEventData.InputButton.Left) {
+				Bresenham3D line = new Bresenham3D(prevpos, Input.mousePosition);
+				foreach (Vector3 point in line) {
+					Vector2 tmp = Camera.main.ScreenToViewportPoint(point);
+					canvastex.SetPixel((int)(tmp.x * 128), (int)(tmp.y * 128), Color.red);
+				}
+				canvastex.Apply();
+			}
+			prevpos = Input.mousePosition;
 		}
-		canvastex.Apply();
-
-		prevpos = Input.mousePosition;
 	}
 
 	public void OnEndDrag(PointerEventData eventData) {
-		brush.position = startPosition;
+		if (eventData.button == PointerEventData.InputButton.Right) {
+			playercontrol.canLookAround = true;
+			brush.position = startPosition;
 
-		Color[] cols = new Color[128 * 128];
-		for (int i = 0; i < 128 * 128; i++) {
-			cols[i] = Color.clear;
+			Color[] cols = new Color[128 * 128];
+			for (int i = 0; i < 128 * 128; i++) {
+				cols[i] = Color.clear;
+			}
+
+			canvastex.SetPixels(cols);
+			canvastex.Apply();
+			dragging = false;
+
+			Cursor.lockState = CursorLockMode.Locked;
 		}
-
-		canvastex.SetPixels(cols);
-		canvastex.Apply();
 	}
 
 }
